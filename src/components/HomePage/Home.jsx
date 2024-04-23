2d2ec6ec1412d065bed69d7e20fd17e15042daec
rahulroynipon <rahulroynipon@gmail.com>
2024-04-24 01:29:29 +0600

    add

--- src/components/HomePage/Home.jsx
+++ src/components/HomePage/Home.jsx
@@ -7,10 +7,15 @@ import Event from "./components/event";
 import Footer from "./components/footer";
 import { useState } from "react";
 
+import Logo from "/src/assets/Logo/cpccu.png";
+import NavIcon from "/src/assets/icons/menu.svg";
+import CloseIcon from "/src/assets/icons/close.svg";
+
 export default function Home() {
   return (
-    <section className="lg:px-28 md:px-12 md:pt-8 overflow-x-hidden flex flex-col">
+    <section className="lg:px-28 overflow-x-hidden flex flex-col h-screen">
       <NavBar />
+
       <Hero />
       <JoinClub />
       <Testimonials />
--- src/components/HomePage/components/nav.jsx
+++ src/components/HomePage/components/nav.jsx
@@ -36,9 +36,8 @@ export default function NavBar() {
 
   return (
     <nav
-      ref={navPosition}
-      className="z-50 bg-header md:rounded-xl text-white px-8 py-2 flex items-center justify-between 
-      fixed top-0 left-0 right-0 md:sticky"
+      className="px-8 md:mt-8 sticky top-0 z-50 h-28 w-full bg-header
+  flex items-center justify-between py-3 text-white"
     >
       <div className="flex items-center justify-center gap-2 cursor-pointer">
         <img className="h-9" src={Logo} alt="CPCCU" />
@@ -46,26 +45,56 @@ export default function NavBar() {
       </div>
       <ul
         ref={navbar}
-        className={`nav ${navClose.open ? "left-0" : "-left-[290px]"}`}
+        className={` ${navClose.open ? "right-0" : "-right-[60%]"}
+         transition-all duration-500  absolute h-screen w-[60%] bg-header/70 top-0 bottom-0 
+        md:bg-transparent md:h-full flex flex-col justify-center items-center
+       md:static md:flex-row md:justify-center md:items-center gap-5 text-white`}
       >
-        <li className="nav-item">Home</li>
-        <li className="nav-item">About Us</li>
-        <li className="nav-item">Contact</li>
-        <li className="nav-item">Home</li>
-        <button className="btn font-semibold md:hidden top-6">
-          Get Started
-        </button>
+        <li>Home</li>
+        <li>Gallery</li>
+        <li>About us</li>
+        <li>Contact</li>
       </ul>
-      <button className="btn font-semibold hidden top-6 md:block">
+
+      <button className="hidden md:block btn font-semibold  top-6">
         Get Started
       </button>
-      <button
-        ref={toggler}
-        onClick={navBarHandler}
-        className="hover:scale-95 transition-all duration-300 md:hidden"
-      >
+
+      <button ref={toggler} onClick={navBarHandler} className="md:hidden">
         <img className="h-7" src={navClose.icon} alt="Navbar" />
       </button>
     </nav>
+    // <nav
+    //   ref={navPosition}
+    //   className="z-50 bg-header md:rounded-xl text-white px-8 py-2 flex items-center justify-between
+    //   fixed top-0 left-0 right-0 md:sticky"
+    // >
+    //   <div className="flex items-center justify-center gap-2 cursor-pointer">
+    //     <img className="h-9" src={Logo} alt="CPCCU" />
+    //     <h1 className="text-2xl font-custom">CPCCU</h1>
+    //   </div>
+    //   <ul
+    //     ref={navbar}
+    //     className={`nav ${navClose.open ? "left-0" : "-left-[290px]"}`}
+    //   >
+    //     <li className="nav-item">Home</li>
+    //     <li className="nav-item">About Us</li>
+    //     <li className="nav-item">Contact</li>
+    //     <li className="nav-item">Home</li>
+    //     <button className="btn font-semibold md:hidden top-6">
+    //       Get Started
+    //     </button>
+    //   </ul>
+    //   <button className="btn font-semibold hidden top-6 md:block">
+    //     Get Started
+    //   </button>
+    //   <button
+    //     ref={toggler}
+    //     onClick={navBarHandler}
+    //     className="hover:scale-95 transition-all duration-300 md:hidden"
+    //   >
+    //     <img className="h-7" src={navClose.icon} alt="Navbar" />
+    //   </button>
+    // </nav>
   );
 }
