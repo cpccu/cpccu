22df83fff6b8177824a8fbb504c8dd15285b11de
rahulroynipon <rahulroynipon@gmail.com>
2024-04-24 02:33:29 +0600

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/HomePage/Home";
 
 function App() {
   return (
     <>
+      {/* <BrowserRouter>
+        <Routes>
+          <Route></Route>
+        </Routes>
+      </BrowserRouter> */}
       <Home />
     </>
   );
--- src/components/HomePage/Home.jsx
+++ src/components/HomePage/Home.jsx
@@ -5,17 +5,11 @@ import OurGallery from "./components/ourGallery";
 import Testimonials from "./components/testimonials";
 import Event from "./components/event";
 import Footer from "./components/footer";
-import { useState } from "react";
-
-import Logo from "/src/assets/Logo/cpccu.png";
-import NavIcon from "/src/assets/icons/menu.svg";
-import CloseIcon from "/src/assets/icons/close.svg";
 
 export default function Home() {
   return (
     <section className="lg:px-28 overflow-x-hidden flex flex-col h-screen">
       <NavBar />
-
       <Hero />
       <JoinClub />
       <Testimonials />
