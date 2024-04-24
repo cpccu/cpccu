44ba6555994863b6703ddfeb431ba4092e2d2ff6
rahulroynipon <rahulroynipon@gmail.com>
2024-04-24 18:10:21 +0600

    add

--- src/components/ContactPage/contact.jsx
+++ src/components/ContactPage/contact.jsx
@@ -1,6 +1,7 @@
 import LiveIcon from "/src/assets/icons/live.png";
 import TeleIcon from "/src/assets/icons/telephone.png";
 import contactIcon from "/src/assets/icons/contact.jpg";
+import heroImg from "/src/assets/others/university.png";
 
 export default function Contact() {
   return (
@@ -28,6 +29,48 @@ export default function Contact() {
 
       {/* contact section end */}
 
+      {/* start from */}
+      <section className="flex gap-5 bg-black/10 px-2 py-2 pb-5 md:px-5 md:pb-6">
+        <form className="flex flex-col w-full gap-3 bg-white p-5 rounded shadow-lg ">
+          <h1 className="text-3xl font-bold font-custom text-black/75">
+            Let's contact
+          </h1>
+          <div className="flex items-center gap-2">
+            <input
+              className="w-full border border-black/30 py-2 px-5 rounded placeholder:font-semibold outline-primary/60 bg-gray-200"
+              type="text"
+              placeholder="Enter your number"
+            />
+            <input
+              className="w-full border border-black/30 py-2 px-5 rounded placeholder:font-semibold outline-primary/60 bg-gray-200"
+              type="email"
+              placeholder="Enter your email"
+            />
+          </div>
+
+          <textarea
+            className="border border-black/30 py-3 px-5 rounded placeholder:font-semibold outline-primary/60 bg-gray-200"
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="Write the message"
+          ></textarea>
+
+          <button
+            className="border self-start px-5 py-2 font-semibold bg-primary hover:bg-primary/85 hover:ring-1 transition-all duration-300
+           ring-primary text-white rounded"
+          >
+            Sent your messsage
+          </button>
+        </form>
+        <div className="w-full border-black overflow-hidden rounded relative hidden md:block shadow-xl">
+          <img className=" object-cover h-full" src={heroImg} alt="" />
+          <div className=" absolute inset-0 bg-header/30"></div>
+        </div>
+      </section>
+      {/* end from  */}
+
       {/* map section start */}
 
       <section>
@@ -47,7 +90,7 @@ export default function Contact() {
 
 function Service({ img, btnName, btnLink }) {
   return (
-    <main className="flex gap-7 border items-center justify-between px-8 py-4 bg-white rounded shadow md:shadow-lg">
+    <main className="flex gap-7 border items-center justify-between px-8 py-4 bg-white rounded shadow md:shadow-lg  hover:scale-105 hover:ring-1 ring-primary/70 transition-all duration-300 ">
       <section className="flex flex-col items-center justify-center">
         <img src={img} alt={btnName} />
         <h1 className="font-semibold text-black/70 my-3 md:hidden">
@@ -66,17 +109,5 @@ function Service({ img, btnName, btnLink }) {
         </p>
       </section>
     </main>
-  );
-}
-
-function FromSection() {
-  return (
-    <from>
-      <input
-        className="py-2 px-3 border-b-2
-       border-black/40 rounded-full outline-none"
-        type="text"
-      />
-    </from>
   );
 }
