b7d6147f79047433eece20e4647ef767f047aef9
mdsiaofficial <mdsiaofficial@gmail.com>
2024-04-25 15:52:14 +0600

    updated Gallery page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/HomePage/Home";
 import Layout from "./components/layout/layout";
 import Contact from "./components/ContactPage/contact";
+import OurGallery from "./components/HomePage/components/ourGallery";
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route path="" element={<Home />}></Route>
-            <Route path="gallery" element={""}></Route>
+            <Route path="gallery" element={<OurGallery></OurGallery>}></Route>
             <Route path="about" element={""}></Route>
             <Route path="contact" element={<Contact />}></Route>
           </Route>
