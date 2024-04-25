724a34d855f9cc585541180da3f7a4aa2e00b82e
mdsiaofficial <mdsiaofficial@gmail.com>
2024-04-25 17:02:33 +0600

    added about us page

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./components/HomePage/Home";
 import Layout from "./components/layout/layout";
 import Contact from "./components/ContactPage/contact";
 import OurGallery from "./components/HomePage/components/ourGallery";
-
+import aboutUs from "./components/HomePage/components/aboutUs";
 function App() {
   return (
     <>
@@ -11,8 +11,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route path="" element={<Home />}></Route>
-            <Route path="gallery" element={<OurGallery></OurGallery>}></Route>
-            <Route path="about" element={""}></Route>
+            <Route path="gallery" element={<OurGallery />}></Route>
+            <Route path="about" element={<aboutUs />}></Route>
             <Route path="contact" element={<Contact />}></Route>
           </Route>
         </Routes>
