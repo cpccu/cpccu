14dfea0fb3c3cbeb3f294bea09ae906c78577fbe
mdsiaofficial <mdsiaofficial@gmail.com>
2024-05-08 08:12:05 +0600

    Change class to className in LayOut.jsx and OurMission.jsx
    This commit changes the usage of "class" to "className" in the files LayOut.jsx and OurMission.jsx. This is

--- src/LayOut.jsx
+++ src/LayOut.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Header from "./components/Global/Header";
+// import Header from "./components/Global/Header";
+import Header from './components/Global/Header.jsx';
 import NavBar from "./components/Global/NavBar";
 import Footer from "./components/Global/Footer";
 import GoToTop from "./components/Global/GoToTop";
