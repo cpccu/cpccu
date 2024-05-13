ec0b101bccea17ee4fdf5e675a5fc95218158665
mdsiaofficial <mdsiaofficial@gmail.com>
2024-05-13 20:20:26 +0600

    Updated BlogPost.json, upcomingEvent.json, UpComingEventCard.jsx, OurMission.jsx, and Home.jsx

--- src/components/HOME/OurMission.jsx
+++ src/components/HOME/OurMission.jsx
@@ -18,7 +18,7 @@ export default function OurMission() {
   return (
     <section
       id="ourMission"
-      class="relative flex items-center justify-end px-[0.2em] lg:px-[2.9em] xl:px-[7em] pt-[20rem] pb-12"
+      class="relative flex items-center justify-end px-[0.2em] lg:px-[2.9em] xl:px-[7em] lg:pt-[20rem] pb-12"
     >
       <main
         class=" px-3 py-5 md:px-10 md:py-12 bg-header/10 ring-header/35 flex flex-col  items-start justify-center gap-10 ring-1 
