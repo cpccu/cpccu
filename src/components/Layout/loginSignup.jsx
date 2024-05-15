f1ced0e954e6a69c47c925f35d5296386c34a9af
mdsiaofficial <mdsiaofficial@gmail.com>
2024-05-15 12:23:08 +0600

    blog post updated

---
+++ src/components/Layout/loginSignup.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+// <FontAwesomeIcon icon={faUser} />
+// <FontAwesomeIcon icon={faEnvelope} />
+// <FontAwesomeIcon icon={faLock} />
+export default function LoginSignup(){
+
+  return (
+    <div className="">
+      
+
+      
+
+
+    </div>
+  )
+}
+
