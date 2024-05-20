2df6b4c0ccd54e8bee6fc2010cd735ed056476d7
mdsiaofficial <mdsiaofficial@gmail.com>
2024-05-20 18:39:48 +0600

    "Updated Login and Signup components with new styles, added FontAwesomeIcon and react-router-dom links, and rearranged layout"

--- src/components/LOGINSIGNUP/Signup.jsx
+++ src/components/LOGINSIGNUP/Signup.jsx
@@ -1,13 +1,41 @@
 import Logo from "./../../assets/logo/cpccu.png";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import { faArrowLeftLong } from "@fortawesome/free-solid-svg-icons";
+import { useContext, useEffect } from "react";
+
 export default function Signup() {
-  const labelCSS = `uppercase font-semibold text-sm text-gray-400 font-custom`;
+  const labelCSS = `uppercase font-semibold text-sm text-gray-800 font-custom`;
   const inputCSS = `outline-none border-b border-gray-300 py-2 focus:border-black`;
   const btn = `uppercase font-semibold h-12 px-1 rounded-full w-full text-sm`;
 
   return (
-    <div className={`bg-white h-svh padding flex px-3 pb-12`}>
-      <main className="mx-auto lg:min-w-[30rem] lg:w-[60rem] lg:max-w-[70rem] flex flex-col gap-14 items-start justify-center padding">
+    <>
+    <Link to="/">
+        <button className="bg-header absolute z-50 left-[2rem] mdd:left-[6rem] mt-[5rem] flex items-center justify-center h-10 rounded-lg lg:w-[10rem] gap-3 px-3 py-2 hover:bg-headerHover trans">
+          <FontAwesomeIcon className=" text-white font-extrabold text-2xl" icon={faArrowLeftLong} />
+          <h1 className="font-bold text-white hidden md:block">Home Page</h1>
+        </button>
+      </Link>
+    
+    <div className={`bg-white h-svh padding flex px-3`}>
+      
+
+      <section
+        className="w-full items-center justify-center hidden lg:flex relative"
+        style={{
+          backgroundImage: `url(https://i.ibb.co/3pzb3VW/R.png)`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <h1 className="text-3xl font-custom text-gray-500 absolute top-[50%] -right-20 -rotate-90">
+          Sign up to Join CPCCU
+        </h1>
+      </section>
+      <main className="mx-auto lg:min-w-[30rem] lg:w-[60rem] lg:max-w-[70rem] flex flex-col gap-4 items-start justify-center padding">
         {/* logo section start */}
         <section className="flex flex-col self-center items-center justify-center gap-2">
           <img className="h-24" src={Logo} alt="Logo" />
@@ -95,20 +123,9 @@ export default function Signup() {
           </p>
         </form>
       </main>
-      <section
-        className="w-full items-center justify-center hidden lg:flex relative"
-        style={{
-          backgroundImage: `url(https://i.ibb.co/3pzb3VW/R.png)`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-        }}
-      >
-        <h1 className="text-3xl font-custom text-gray-500 absolute top-[50%] -left-32 -rotate-90">
-          Login to Access Dashboard
-        </h1>
-      </section>
+
     </div>
+    </>
   );
 }
 
