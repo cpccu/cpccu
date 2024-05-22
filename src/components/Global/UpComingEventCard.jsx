38b85772a040322b0a05c3c293f7d5630aa91f3e
mdsiaofficial <mdsiaofficial@gmail.com>
2024-05-22 11:24:38 +0600

    "Updated styles and layout for UpComingEventCard component"

--- src/components/Global/UpComingEventCard.jsx
+++ src/components/Global/UpComingEventCard.jsx
@@ -6,12 +6,12 @@ const UpComingEventCard = ({ data, clName }) => {
   return (
     <main
       className={cn(
-        "relative grid md:grid-cols-6 lg:grid-cols-7 xl:grid-cols-8 mxl:grid-cols-8 mmmxl:grid-cols-10 gap-7 lg:gap-5 cursor-default overflow-hidden",
+        "relative grid md:grid-cols-6 lg:grid-cols-7 xl:grid-cols-8 mxl:grid-cols-8 mmmxl:grid-cols-10 gap-7 lg:gap-5 mmmxl:gap-10 cursor-default overflow-hidden",
         clName
       )}
     >
       {/* Picture Section */}
-      <section className="md:col-span-3 lg:col-span-4 xl:col-span-3 mxl:col-span-3 mmmxl:col-span-4 md:h-[20rem] lg:h-[28rem]  mxl:h-auto mmmxl:h-auto xl:h-auto overflow-hidden">
+      <section className="md:col-span-3 lg:col-span-4 xl:col-span-3 mxl:col-span-3 mmmxl:col-span-3 md:h-[20rem] lg:h-[28rem]  mxl:h-auto mmmxl:h-[26rem] xl:h-auto overflow-hidden">
         <img
           className="h-full w-full object-cover"
           src={data?.img}
@@ -23,35 +23,35 @@ const UpComingEventCard = ({ data, clName }) => {
       {/* Picture Section End*/}
 
       {/* Content Section */}
-      <section className="h-full md:col-span-3 lg:col-span-3 xl:col-span-5 mxl:col-span-5 mmmxl:col-span-6 flex flex-col items-start gap-4">
+      <section className="h-full md:col-span-3 lg:col-span-3 xl:col-span-5 mxl:col-span-5 mmmxl:col-span-7 flex flex-col items-start gap-4">
         <TimeBox date={data?.date} />
-        <h1 className="text-2xl lg:text-3xl xl:text-5xl  font-semibold lg:line-clamp-none">
-          {data?.eventHeadLine1}
+        <h1 className="text-2xl lg:text-3xl xl:text-4xl  font-semibold lg:line-clamp-none">{`"`}
+          {data?.eventHeadLine1}{`"`}
         </h1>
-        <p className="font-[450] lg:line-clamp-3">{data?.textContext}</p>
+        <p className="font-[450] text-xl lg:line-clamp-3">{data?.textContext}</p>
 
-        {/* price */}
-        <h1 className="text-2xl hidden lg:block font-semibold lg:line-clamp-1">
+        {/* reward */}
+        <h1 className="text-3xl hidden lg:block font-semibold lg:line-clamp-1">
           {data?.eventHeadLine2}
         </h1>
-        <p className="font-[450] hidden lg:block lg:line-clamp-3">{data?.reward}</p>
+        <p className="font-[450] text-2xl hidden lg:block lg:line-clamp-3">{data?.reward}</p>
 
-        <h1 className="text-2xl hidden lg:hidden mmmxl:block font-semibold lg:line-clamp-1">
+        <h1 className="text-2xl hidden lg:hidden mmmxl:hidden font-semibold lg:line-clamp-1">
           {data?.eventHeadLine3}
         </h1>
-        <p className="font-[450] hidden lg:hidden mmmxl:block  lg:line-clamp-3">
+        <p className="font-[450] hidden lg:hidden mmmxl:hidden  lg:line-clamp-3">
           {" -> "}
           {data?.rules1}
         </p>
-        <p className="font-[450] hidden lg:hidden mmmxl:block  lg:line-clamp-3">
+        <p className="font-[450] hidden lg:hidden mmmxl:hidden  lg:line-clamp-3">
           {" -> "}
           {data?.rules2}
         </p>
-        <p className="font-[450] hidden lg:hidden mmmxl:block  lg:line-clamp-3">
+        <p className="font-[450] hidden lg:hidden mmmxl:hidden  lg:line-clamp-3">
           {" -> "}
           {data?.rules3}
         </p>
-        <p className="font-[450] hidden lg:hidden mmmxl:block  lg:line-clamp-3">
+        <p className="font-[450] hidden lg:hidden mmmxl:hidden  lg:line-clamp-3">
           {" -> "}
           {data?.rules4}
         </p>
