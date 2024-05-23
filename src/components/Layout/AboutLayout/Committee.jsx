1b92da66e50be5a2115a1b92a5e0004bca7d259e
rahulroynipon <rahulroynipon@gmail.com>
2024-05-24 02:31:02 +0600

    add the alumni page

--- src/components/Layout/AboutLayout/Committee.jsx
+++ src/components/Layout/AboutLayout/Committee.jsx
@@ -1,9 +1,12 @@
-import AboutCardLayout from "../../ABOUT/AboutCardLayout";
+import AboutCard from "../../ABOUT/AboutCard";
+import Data from "./../../../../data/Committee.json";
 
-export default function Committee({ setIDname }) {
+export default function Committee() {
   return (
-    <>
-      <AboutCardLayout setIDname={setIDname} />
-    </>
+    <main className="grid md:grid-cols-2 lg:grid-cols-4 gap-10 lg:gap-3 xl:gap-10 padding py-16 bg-responsibility">
+      {Data
+        ? Data.map((item, index) => <AboutCard key={index} Data={item} />)
+        : null}
+    </main>
   );
 }
